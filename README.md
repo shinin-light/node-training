d42d6125ae92274906e87e2e5ffdfada5159c1a6
Lorenzo Donati <lorenzo.donati@snyk.io>
2021-06-24 15:24:02 +0200

    Tested

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # node-training
+
+- `git clone` this repo
+- `cd node-training` 
+- `npm i`
+- `npm run start`
+- Open a webpage on (http://localhost:3456/)[http://localhost:3456/]
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -5,10 +6,20 @@ const port = 3456;
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.header({"Access-Control-Allow-Origin": "*"});
+  next();
+})  
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client.html'));
+});
+
 app.post('/echo', (req, res) => {
-  console.log(`Received request with body: ${req.body}`);
+  console.log(`Received request with body:`, req.body);
   const content = req.body.content;
-  res.status(200).json({ content });
+  res.status(200);
+  res.json({ content });
 });
 
 app.listen(port, () => console.log(`Server listening at http://localhost:${port}`));
