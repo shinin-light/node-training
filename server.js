ab5d46be148c56647ef43f2440688f959a3ccb3d
Lorenzo Donati <lorenzo.ff.donati@gmail.com>
2021-06-24 13:19:49 +0200

    Created server

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.post('/echo', (req, res) => {
+  console.log(`Received request with body: ${req.body}`);
+  const content = req.body.content;
+  res.status(200).json({ content });
+});
+
+app.listen(port, () => console.log(`Server listening at http://localhost:${port}`));
